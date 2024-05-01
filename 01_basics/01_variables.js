b734077a38f518959c31d2ade47d03db7a1a0e50
alok_08 <100188982+Aloksongara08@users.noreply.github.com>
2024-05-01 15:52:58 +0000

    step 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "aloksongara@gmail.com"
+var accountPassword ="12345"
+acccountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 //not allowed
+accountEmail="huuhduhw@hc.com"
+accountPassword = "12345"
+accountCity="Benglurure"
+
+
+console.log(accountId);
+
+console.table([accountId , accountEmail,accountPassword,accountCity,accountState]);
